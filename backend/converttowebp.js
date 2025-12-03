57e9f87d2cc80dba0060709e3cfd643c2cc09323
Eirikfin <148289492+Eirikfin@users.noreply.github.com>
2025-12-03 14:56:50 +0100

    Chad added

--- backend/converttowebp.js
+++ backend/converttowebp.js
@@ -2,8 +2,8 @@ import sharp from "sharp";
 import fs from "fs-extra";
 import path from "path";
 
-const inputFolder = "./public/images/countries/asia/yemen";      // folder with your original images
-const outputFolder = "./public/images/countries/asia/yemen";          // folder to save .webp files
+const inputFolder = "./public/images/inventions/africa/chad";      // folder with your original images
+const outputFolder = "./public/images/inventions/africa/chad";          // folder to save .webp files
 
 async function convertImages() {
   await fs.ensureDir(outputFolder);
